15ee0d977ca77022cfc916c505fc9533bf89f541
YTRedstone <josh@thegamehubmc.net>
2021-05-12 19:04:55 -0700

    2

--- src/main/java/com/joshyspcs/betterbaratone/Main.java
+++ src/main/java/com/joshyspcs/betterbaratone/Main.java
@@ -6,7 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 import org.apache.logging.log4j.Logger;
 
